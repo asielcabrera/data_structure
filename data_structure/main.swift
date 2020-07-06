c88b2633a5ad29bcf6b4af655ce05bacb79e3c22
Asiel Cabrera <cabrerasiel@gmail.com>
2020-07-06 01:13:42 -0400

    Initial Commit

---
+++ data_structure/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  data_structure
+//
+//  Created by Asiel Cabrera on 7/6/20.
+//  Copyright © 2020 Asiel Cabrera. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
